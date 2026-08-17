5c75620c549d46562aa0e5c0d1685757b5019ede
mateo <mateoblondetz@gmail.com>
2026-08-17 15:56:18 -0500

    feat: add comments for better readibility to functions

--- blockchain.py
+++ blockchain.py
@@ -1,25 +1,32 @@
+#Initializing our blockchain list
 blockchain = []
 
 def get_last_blockchain_value():
+  """Returns the last value of the blockchain"""
   return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+  """Append a new value and the last transaction to the blockchain"""
   blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
+  """Gets the input from the user and returns it"""
   user_input = float(input("Your transaction amount please: "))
   return user_input
 
-
+#Gets first transaction
 tx_amount = get_user_input()
-add_value(2)
+add_value(tx_amount)
 
+#Gets second transaction
 tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+add_value(last_transaction=get_last_blockchain_value(),
+           transaction_amount=tx_amount)
 
+#Get thrd transaction
 tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+add_value(tx_amount, get_last_blockchain_value())
 
 print(blockchain)
