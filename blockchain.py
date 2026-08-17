2a966df5c5c8138184b8d7b91fbe59218dde825b
mateo <mateoblondetz@gmail.com>
2026-08-17 16:23:10 -0500

    feat: add exit loop functionality

--- blockchain.py
+++ blockchain.py
@@ -40,7 +40,11 @@ while True:
   if user_choice == "1":
     tx_amount = get_transaction_value()
     add_value(tx_amount, get_last_blockchain_value())
-  else:
+  elif user_choice == "2":
     print_blockchain_elements()
+  elif user_choice == "q":
+    break
+  else:
+    print("Invalid input")
 
 print('Completed')
