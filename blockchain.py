81cac2a1778ca9855b60c3c8b9aee860d82dfd78
mateo <mateoblondetz@gmail.com>
2026-08-17 23:21:29 -0500

    feat: add validation

--- blockchain.py
+++ blockchain.py
@@ -3,11 +3,15 @@ blockchain = []
 
 def get_last_blockchain_value():
   """Returns the last value of the blockchain"""
+  if len(blockchain) < 1:
+    return None
   return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
   """Append a new value and the last transaction to the blockchain"""
+  if last_transaction == None:
+    last_transaction = [1]
   blockchain.append([last_transaction, transaction_amount])
 
 
