d1cdc1c9e621e0f32fbc613563f39338af03628e
mateo <mateoblondetz@gmail.com>
2026-08-20 10:28:10 -0500

    feat: add verify transactions feature

--- blockchain.py
+++ blockchain.py
@@ -109,6 +109,10 @@ def verify_chain():
     return True
 
 
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transactions])
+
+
 waiting_for_input = True
 
 while waiting_for_input:
@@ -117,6 +121,7 @@ while waiting_for_input:
     print("2: Mine a new block")
     print("3: Output the blockchains blocks")
     print("4: Output participants")
+    print("5: Check transaction validity")
     print("h: Manipulate the chain")
     print("e: Exit")
     user_choice = get_user_choice()
@@ -136,6 +141,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == "4":
         print(participants)
+    elif user_choice == "5":
+        if verify_transactions():
+            print("All transactions are valid")
+        else:
+            print("There are invalid transactions")
     elif user_choice == "h":
         if len(blockchain) >= 1:
             blockchain[0] = {
