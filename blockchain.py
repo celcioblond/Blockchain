03b5bee0306d842df9493c46832a16b60632e404
mateo <mateoblondetz@gmail.com>
2026-08-24 12:06:00 -0500

    refactor: reemplazar la función hash_block con hash_string_256 para el cálculo de hash

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,8 @@ import json
 from collections import OrderedDict
 from functools import reduce
 
+from hash_util import hash_block, hash_string_256
+
 # Initializing our blockchain list
 MINING_REWARD = 10
 
@@ -17,9 +19,6 @@ blockchain = [genesis_block]
 open_transactions = []
 owner = "Celcio"
 participants = {'Celcio'}
-
-def hash_block(block):
-    return hashlib.sha256(json.dumps(block, sort_keys=True).encode()).hexdigest()
 
 
 def get_balance(participant):
@@ -41,7 +40,7 @@ def get_last_blockchain_value():
 
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
-    guess_hash = hashlib.sha256(guess).hexdigest()
+    guess_hash = hash_string_256(guess)
     print(guess_hash)
     return guess_hash[0:2] == "00"
 
