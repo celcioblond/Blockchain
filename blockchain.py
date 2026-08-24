d22183b2c531fa2ff9210b3070caaba6a49541e8
mateo <mateoblondetz@gmail.com>
2026-08-24 11:50:22 -0500

    feat: implement proof of work and improve transaction handling with OrderedDict

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
-import functools
 import hashlib
 import json
+from collections import OrderedDict
+from functools import reduce
 
 # Initializing our blockchain list
 MINING_REWARD = 10
@@ -9,6 +10,7 @@ genesis_block = {
     "previous_hash": "",
     "index": 0,
     "transactions": [],
+    "proof": 100
 }
 
 blockchain = [genesis_block]
@@ -17,16 +19,16 @@ owner = "Celcio"
 participants = {'Celcio'}
 
 def hash_block(block):
-    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
+    return hashlib.sha256(json.dumps(block, sort_keys=True).encode()).hexdigest()
 
 
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+    amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+    amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
     return amount_received - amount_sent
 
 
@@ -37,6 +39,20 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
+def valid_proof(transactions, last_hash, proof):
+    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    print(guess_hash)
+    return guess_hash[0:2] == "00"
+
+
+def proof_of_work():
+    last_block = blockchain[-1]
+    last_hash = hash_block(last_block)
+    proof = 0
+    while not valid_proof(open_transactions, last_hash, proof):
+        proof += 1
+    return proof
 
 def verify_transaction(transaction):
     sender_balance = get_balance(transaction['sender'])
@@ -48,7 +64,8 @@ def verify_transaction(transaction):
 
 def add_transaction(recipient, sender=owner, amount=1):
     """Append a new value and the last transaction to the blockchain"""
-    transaction = {"sender": sender, "recipient": recipient, "amount": amount}
+    # transaction = {"sender": sender, "recipient": recipient, "amount": amount}
+    transaction = OrderedDict([('sender', sender), ('recipient', recipient), ('amount', amount)])
     if verify_transaction(transaction):
       open_transactions.append(transaction)
       participants.add(sender)
@@ -62,17 +79,20 @@ def mine_block():
     """Add new block to blockchain"""
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
-    reward_transaction = {
-        'sender': 'MINING',
-        'recipient': owner,
-        'amount': MINING_REWARD,
-    }
+    proof = proof_of_work()
+    # reward_transaction = {
+    #     'sender': 'MINING',
+    #     'recipient': owner,
+    #     'amount': MINING_REWARD,
+    # }
+    reward_transaction = OrderedDict([('sender', 'MINING'), ('recipient', owner), ('amount', MINING_REWARD)])
     copied_transactions = open_transactions[:]
     copied_transactions.append(reward_transaction)
     block = {
         "previous_hash": hashed_block,
         "index": len(blockchain),
         "transactions": copied_transactions,
+        "proof": proof
     }
     blockchain.append(block)
     return True
@@ -104,6 +124,9 @@ def verify_chain():
           continue
         if block['previous_hash'] != hash_block(blockchain[index - 1]):
           return False
+        if not valid_proof(block['transactions'][:-1], block['previous_hash'], block['proof']):
+            print("Proof of work is invalid")
+            return False
     return True
 
 
