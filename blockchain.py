4ff497e6798cb92be863d4ffaf0041ad7c1651d1
mateo <mateoblondetz@gmail.com>
2026-09-01 17:46:49 -0500

    feat: eliminar la variable participants y ajustar el menú de opciones

--- blockchain.py
+++ blockchain.py
@@ -14,7 +14,6 @@ MINING_REWARD = 10
 blockchain = []
 open_transactions = []
 owner = "Celcio"
-participants = {"Celcio"}
 
 
 def load_data():
@@ -228,8 +227,7 @@ while waiting_for_input:
     print("1: Add a new transaction value")
     print("2: Mine a new block")
     print("3: Output the blockchains blocks")
-    print("4: Output participants")
-    print("5: Check transaction validity")
+    print("4: Check transaction validity")
     print("e: Exit")
     user_choice = get_user_choice()
     if user_choice == "1":
@@ -248,8 +246,6 @@ while waiting_for_input:
     elif user_choice == "3":
         print_blockchain_elements()
     elif user_choice == "4":
-        print(participants)
-    elif user_choice == "5":
         if verify_transactions():
             print("All transactions are valid")
         else:
