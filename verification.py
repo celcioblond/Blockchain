4397e8d0816a6428781c9820d68427cfbeca0f4b
mateo <mateoblondetz@gmail.com>
2026-09-02 21:28:41 -0500

    feat: refactor la clase Verification para utilizar métodos estáticos y de clase

--- verification.py
+++ verification.py
@@ -2,8 +2,8 @@ from hash_util import hash_block, hash_string_256
 
 
 class Verification:
-
-    def valid_proof(self, transactions, last_hash, proof):
+    @staticmethod
+    def valid_proof(transactions, last_hash, proof):
         guess = (
             str([tx.to_ordered_dict() for tx in transactions])
             + str(last_hash)
@@ -12,28 +12,31 @@ class Verification:
         guess_hash = hash_string_256(guess)
         return guess_hash[0:2] == "00"
 
-    def verify_chain(self, blockchain):
+    @classmethod
+    def verify_chain(cls, blockchain):
         """Verify the current blockchain"""
         for index, block in enumerate(blockchain):
             if index == 0:
                 continue
             if block.previous_hash != hash_block(blockchain[index - 1]):
                 return False
-            if not self.valid_proof(
+            if not cls.valid_proof(
                 block.transactions[:-1], block.previous_hash, block.proof
             ):
                 print("Proof of work is invalid")
                 return False
         return True
 
-    def verify_transaction(self, transaction, get_balance):
-        sender_balance = get_balance(transaction.sender)
+    @staticmethod
+    def verify_transaction(transaction, get_balance):
+        sender_balance = get_balance()
         if sender_balance >= transaction.amount:
             return True
         else:
             return False
 
-    def verify_transactions(self, open_transactions, get_balance):
+    @classmethod
+    def verify_transactions(cls, open_transactions, get_balance):
         return all(
-            [self.verify_transaction(tx, get_balance) for tx in open_transactions]
+            [cls.verify_transaction(tx, get_balance) for tx in open_transactions]
         )
