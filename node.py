a49fd7f1fa09c38c57a63a2a0540bfd7dade89d1
mateo <mateoblondetz@gmail.com>
2026-09-04 17:30:08 -0500

    feat: implementar endpoint para obtener la cadena de bloques y manejar respuestas vacías

--- node.py
+++ node.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
+from blockchain import Blockchain
 from wallet import Wallet
 
 app = FastAPI()
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +18,14 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def get_ui():
-    return {"response": "Hello world"}
+@app.get("/chain")
+async def get_chain():
+    chain_snapshot = blockchain.get_chain()
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block["transactions"] = [tx.__dict__ for tx in dict_block["transactions"]]
+
+    if not dict_chain:
+        return JSONResponse(content={"error": "Empty chain"}, status_code=404)
+
+    return JSONResponse(content=dict_chain, status_code=200)
