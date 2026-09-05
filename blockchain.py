6d21495f9c15dc5725a17f9c2c8befc2faee6c15
mateo <mateoblondetz@gmail.com>
2026-09-05 14:01:48 -0500

    feat: agregar retorno None en get_balance si no hay nodo anfitrión

--- blockchain.py
+++ blockchain.py
@@ -111,6 +111,8 @@ class Blockchain:
         return proof
 
     def get_balance(self):
+        if self.hosting_node == None:
+            return None
         participant = self.hosting_node
         tx_sender = [
             [tx.amount for tx in block.transactions if tx.sender == participant]
