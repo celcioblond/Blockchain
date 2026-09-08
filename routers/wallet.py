1559e2485817e0ce6df8d8dab7eb225a1b828f03
mateo <mateoblondetz@gmail.com>
2026-09-08 10:38:40 -0500

    feat: agregar funciones para crear, cargar claves y obtener saldo en el archivo wallet.py

---
+++ routers/wallet.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
+
+from core import state
+from models.blockchain import Blockchain
+
+router = APIRouter()
+
+
+@router.post("/wallet", status_code=status.HTTP_201_CREATED)
+async def create_keys():
+    state.wallet.create_keys()
+    if state.wallet.save_keys():
+        state.blockchain = Blockchain(state.wallet.public_key)
+        response = {
+            "public_key": state.wallet.public_key,
+            "private_key": state.wallet.private_key,
+            "funds": state.blockchain.get_balance(),
+        }
+        return JSONResponse(content=response, status_code=status.HTTP_201_CREATED)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Saving the key failed"
+        )
+
+
+@router.get("/wallet", status_code=status.HTTP_200_OK)
+async def load_keys():
+    if not state.wallet.load_keys():
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Loading the keys failed",
+        )
+
+    state.blockchain = Blockchain(state.wallet.public_key)
+
+    return JSONResponse(
+        content={
+            "public_key": state.wallet.public_key,
+            "private_key": state.wallet.private_key,
+            "funds": state.blockchain.get_balance(),
+        },
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.get("/balance", status_code=status.HTTP_200_OK)
+async def get_balance():
+    balance = state.blockchain.get_balance()
+    if balance != None:
+        response = {
+            "message": "Fetched balance succesfully",
+            "balance": balance,
+        }
+        return JSONResponse(content=response, status_code=status.HTTP_200_OK)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "message": "Loading balance failed",
+                "wallet_set_up": state.wallet.public_key != None,
+            },
+        )
