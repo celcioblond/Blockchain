3746beb2d92a39b6b4a9d476b3fe70e9576db3a0
mateo <mateoblondetz@gmail.com>
2026-09-09 11:00:13 -0500

    feat: agregar rutas para gestión de nodos en el archivo node.py

---
+++ routers/node.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
+
+from core import state
+from schemas.node_request import NodeRequest
+
+router = APIRouter()
+
+
+@router.post("/node", status_code=status.HTTP_201_CREATED)
+async def add_node(node: NodeRequest):
+    if not node:
+        raise HTTPException(status_code=400, detail="No data attached")
+
+    state.blockchain.add_peer_note(node.node)
+
+    response = {
+        "message": "Node added successfully",
+        "all_nodes": state.blockchain.get_peer_nodes(),
+    }
+    return JSONResponse(content=response, status_code=status.HTTP_201_CREATED)
+
+
+@router.get("/node", status_code=status.HTTP_200_OK)
+async def get_nodes():
+    nodes = state.blockchain.get_peer_nodes()
+    return JSONResponse(content=nodes, status_code=status.HTTP_200_OK)
