4170c13ba4408099cc15f1b09196ae4b93c352af
mateo <mateoblondetz@gmail.com>
2026-09-10 17:50:25 -0500

    fix: cambiar la función add_transaction a síncrona

--- routers/transactions.py
+++ routers/transactions.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 
 @router.post("/transaction", status_code=status.HTTP_201_CREATED)
-async def add_transaction(transaction: TransactionRequest):
+def add_transaction(transaction: TransactionRequest):
     if state.wallet.public_key is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="No wallet set up"
