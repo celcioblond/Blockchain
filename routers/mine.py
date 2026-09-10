183010a8ce3b4e1289ae2f42e81d7859629ab4e6
mateo <mateoblondetz@gmail.com>
2026-09-10 17:04:31 -0500

    fix: mejorar manejo de conflictos y validación al añadir bloques

--- routers/mine.py
+++ routers/mine.py
@@ -9,6 +9,9 @@ router = APIRouter()
 
 @router.post("/mine", status_code=status.HTTP_201_CREATED)
 async def mine():
+    if state.blockchain.resolve_conflicts:
+        response = {"message": "Resolve conflicts first, block not added!"}
+        raise HTTPException(status_code=409, detail=response)
     block = state.blockchain.mine_block()
     if block != None:
         dict_block = block.__dict__.copy()
@@ -31,8 +34,14 @@ async def broadcast_block(request: BroadcastBlockRequest):
     last_block = state.blockchain.get_chain()[-1]
 
     if block.index == last_block.index + 1:
-        state.blockchain.add_block(block)
+        if state.blockchain.add_block(block):
+            response = {"message": "Block added"}
+            return JSONResponse(content=response, status_code=status.HTTP_201_CREATED)
+        else:
+            raise HTTPException(status_code=409, detail="invalid")
     elif block.index > last_block.index:
-        pass
+        response = {"message": "Blockchain seems to differ "}
+        state.blockchain.resolve_conflicts = True
+        return JSONResponse(content=response, status_code=status.HTTP_200_OK)
     else:
         raise HTTPException(status_code=409, detail="Invalid data")
